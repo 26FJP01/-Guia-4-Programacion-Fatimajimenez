621e44d307fe4921b3371aba8e969c4df18c1a36
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-05-09 16:50:35 -0600

    Add files via upload

---
+++ Act1_Lecc10_Inciso1.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const int TOTAL_SELLOS = 8;
+        Console.WriteLine("-----Bienvenido a la Barberia---------");
+        for (int i = 1; i <= TOTAL_SELLOS; i++)
+        {
+            Console.WriteLine("\nSello actual{0} de {1}", i, TOTAL_SELLOS);
+            Console.WriteLine("Presione cualquier tecla para registrar esta");
+            Console.ReadKey();
+            Console.WriteLine(">>sellos#{0} registrar correctamente ....");
+        }
+        Console.WriteLine(">>sellos#{0} registrar correctamente ....");
+        Console.WriteLine("¡CUPONERA CPMPLETA!");
+        Console.WriteLine("has ganado tu corte de cabello gratis");
+        Console.WriteLine("\n*****************************");
+        Console.WriteLine("\nPresione una tecla para cerrar");
+        Console.ReadKey();
+    }
+}
---
+++ Act1_Lecc10_Inciso1.psc
@@ -0,0 +1,12 @@
+Algoritmo sin_titulo
+	definir i como entero
+	escribir"-----------CUPONERA DE BARBERIA DON CRUP----------"
+	Para I=1 Hasta 8 Con Paso 1 Hacer
+		escribir"Presione una tecla para marcar el sello ,numero"
+		esperar Tecla
+		escribir"¡SELL0!",i,"marcado.."
+		escribir"--------------------------"
+	Fin Para
+	escribir"¡¡Felicidades has completado los 8 sellos!!"
+	escribir"corte gratis "
+FinAlgoritmo
