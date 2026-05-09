2fdf9cec3277b092585f04451036c102d8b25b22
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-05-09 17:26:13 -0600

    Add files via upload

---
+++ Act2_Lecc9_Inciso2.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+            Console.WriteLine("========================================");
+            Console.WriteLine("   LECCION 09 - ACTIVIDAD 2 - INCISO 2");
+            Console.WriteLine("          SISTEMA DE MONEDAS");
+            Console.WriteLine("========================================");
+            int monedasActuales = 0;
+            int monedasGanadas = 0;
+            Console.Write("Introduce tus monedas actuales: ");
+            monedasActuales = int.Parse(Console.ReadLine());
+            while (monedasActuales < 350)
+            {
+                Console.WriteLine($"\nActualmente tienes {monedasActuales} monedas.");
+                Console.WriteLine("Aun no alcanzas las 350 para el Nivel 5.");
+
+                Console.Write("¿Cuantas monedas ganaste en esta partida?: ");
+                monedasGanadas = int.Parse(Console.ReadLine());
+                monedasActuales += monedasGanadas;
+                Console.WriteLine("Actualizando progreso...");
+            }
+            Console.WriteLine("\n========================================");
+            Console.WriteLine("¡Felicidades! Nivel 5 desbloqueado.");
+            Console.WriteLine("Total final: " + monedasActuales + " monedas.");
+            Console.WriteLine("========================================");
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+
---
+++ Act2_Lecc9_Inciso2.psc
@@ -0,0 +1,18 @@
+Algoritmo Act2_Lecc9_Inciso2
+	Escribir "****************************************"
+    Escribir "   LECCION 09 - ACTIVIDAD 2 - INCISO 2"
+    Escribir "     PASOS DEL DIAGRAMA DE FLUJO"
+    Escribir "****************************************"
+    Escribir "El siguiente texto describe la lógica del programa:"
+    Escribir ""
+    Escribir "Inicio"
+    Escribir "  Solicitar cantidad inicial de monedas"
+    Escribir "  Mientras el total de monedas sea menor a 350 hacer"
+    Escribir "      Preguntar cuántas monedas nuevas se ganaron"
+    Escribir "      Agregar las monedas nuevas al total acumulado"
+    Escribir "      Imprimir el total actualizado en pantalla"
+    Escribir "  Fin Mientras"
+    Escribir "  Imprimir mensaje: ¡Nivel 5 desbloqueado con éxito!"
+    Escribir "Fin"
+    Esperar Tecla
+FinAlgoritmo
