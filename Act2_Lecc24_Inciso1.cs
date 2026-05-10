33683317995e321240e00dc159545610b7721a2e
26FJP01 <jimenezfatimaa16db@gmail.com>
2026-05-09 18:09:57 -0600

    Add files via upload

---
+++ Act2_Lecc24_Inciso1.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("========================================");
+        Console.WriteLine("   LECCION 24 - ACTIVIDAD 2 - INCISO 1");
+        Console.WriteLine("  20 MULTIPLOS DE UN NUMERO DADO - FOR");
+        Console.WriteLine("========================================");
+        Console.Write("Ingrese un numero: ");
+        int numero = int.Parse(Console.ReadLine());
+        int resultado;
+        for (int contador = 1; contador <= 20; contador++)
+        {
+            resultado = numero * contador;
+            Console.WriteLine("Progreso del ciclo: multiplo " + contador + " de 20");
+            Console.WriteLine(numero + " x " + contador + " = " + resultado);
+        }
+        Console.WriteLine("Proceso terminado.");
+        Console.WriteLine("Presione una tecla para salir...");
+        Console.ReadKey();
+    }
+
+}
---
+++ Act2_Lecc24_Inciso1.psc
@@ -0,0 +1,17 @@
+Algoritmo Act2_Lecc24_Inciso1
+Escribir "========================================"
+Escribir "   LECCION 24 - ACTIVIDAD 2 - INCISO 1"
+Escribir "  20 MULTIPLOS DE UN NUMERO DADO - PARA"
+Escribir "========================================"
+Definir numero, contador, resultado Como Entero
+Escribir "Ingrese un numero: " Sin Saltar
+Leer numero
+Para contador <- 1 Hasta 20 Con Paso 1 Hacer
+resultado <- numero * contador
+Escribir "Progreso del ciclo: multiplo ", contador, " de 20"
+Escribir numero, " x ", contador, " = ", resultado
+FinPara
+Escribir "Proceso terminado."
+Esperar Tecla
+FinAlgoritmo
+
